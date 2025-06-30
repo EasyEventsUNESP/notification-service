f9cb1f27dbef02fd541226ef2e7fc277641ccfb9
FelipeMDB-UNESP <felipe.britto@unesp.br>
2025-06-30 09:48:46 -0300

    Add event invitation email functionality
    Introduced EventEmailRequest DTO and a new endpoint to send event invitation emails to multiple recipients. Updated NotificationService to handle event-specific email content and changed the server port to 3003 in application properties.

--- src/main/java/com/easyevents/notification_service/controller/NotificationController.java
+++ src/main/java/com/easyevents/notification_service/controller/NotificationController.java
@@ -1,11 +1,14 @@
 package com.easyevents.notification_service.controller;
 
 import com.easyevents.notification_service.domain.dto.request.EmailRequest;
+import com.easyevents.notification_service.domain.dto.request.EventEmailRequest;
 import com.easyevents.notification_service.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/notification")
@@ -18,5 +21,10 @@ public class NotificationController {
     @PostMapping("/enviar")
     public ResponseEntity<String> send(@RequestBody EmailRequest emailRequest) {
         return notificationService.sendEmails(emailRequest);
+    }
+
+    @PostMapping("/enviar/criacao-evento")
+    public ResponseEntity<String> sendEventEmails(@RequestBody EventEmailRequest eventEmailRequest) {
+        return notificationService.sendEventEmails(eventEmailRequest);
     }
 }
--- src/main/java/com/easyevents/notification_service/service/NotificationService.java
+++ src/main/java/com/easyevents/notification_service/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.easyevents.notification_service.service;
 
 import com.easyevents.notification_service.domain.dto.request.EmailRequest;
+import com.easyevents.notification_service.domain.dto.request.EventEmailRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NotificationService {
@@ -35,5 +38,30 @@ public class NotificationService {
 
         mailSender.send(message);
         logger.info("Email sent successfully to {}", toEmail);
+    }
+
+    public ResponseEntity<String> sendEventEmails(EventEmailRequest eventEmailRequest) {
+        String subject = "Convite para " + eventEmailRequest.getEventTitle();
+        String eventoId = eventEmailRequest.getEventoId();
+        String eventTitle = eventEmailRequest.getEventTitle();
+        String organizerName = eventEmailRequest.getOrganizerName();
+        String eventDateTime = eventEmailRequest.getEventDateTime();
+        String eventLocation = eventEmailRequest.getEventLocation();
+
+        for (String recipient : eventEmailRequest.getEmailList()) {
+            String body = String.format(
+                    "Olá,\n\nVocê foi convidado para participar do evento \"%s\", organizado por %s.\n\n" +
+                            "Detalhes do evento:\n" +
+                            "Data e Hora: %s\n" +
+                            "Local: %s\n\n" +
+                            "Por favor, confirme ou recuse sua presença ao clickar nos links abaixo:\n\n" +
+                            "Confirmar presença: http://localhost:3002/guest/confirmar/%s/%s\n" +
+                            "Negar presença: http://localhost:3002/guest/negar/%s/%s\n\n" +
+                            "Aguardamos ansiosamente sua resposta!",
+                    eventTitle, organizerName, eventDateTime, eventLocation, eventoId, recipient, eventoId, recipient
+            );
+            sendEmail(recipient, subject, body);
+        }
+        return ResponseEntity.ok("Emails enviados com sucesso para os convidados!");
     }
 }
